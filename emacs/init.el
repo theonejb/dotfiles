99e5aa2968f0465285c19bd189c42353210f9a39
Asad Jibran Ahmed <surfer.a1@gmail.com>
2016-12-21 22:15:05 +0400

    Added packages and some more stuff

--- emacs/init.el
+++ emacs/init.el
@@ -30,7 +30,10 @@
 				(venv-workon)
 				(jedi:stop-server))
 			      (local-set-key (kbd "C-c v") 'workon-setup)))
-(setq mode-line-format (cons '(:exec venv-current-name) mode-line-format))			   
+(setq mode-line-format (cons '(:exec venv-current-name) mode-line-format))
+
+(venv-initialize-interactive-shells)
+(venv-initialize-eshell)
 
 (setq jedi:complete-on-dot t)
 (setq jedi:environment-root "jedi-p2")
@@ -53,6 +56,9 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
 
 (add-hook 'web-mode-hook (lambda ()
 			   (emmet-mode)))
@@ -88,3 +94,15 @@
 (global-set-key (kbd "C-c n") (lambda ()
 				(interactive)
 				(find-file "/Users/asadjb/Dropbox/orgmode/notes.org")))
+
+(global-set-key (kbd "C-=") 'er/expand-region)
+
+(defun duplicate-line()
+  (interactive)
+  (move-beginning-of-line 1)
+  (kill-line)
+  (yank)
+  (open-line 1)
+  (next-line 1)
+  (yank))
+(global-set-key (kbd "C-c C-d") 'duplicate-line)
